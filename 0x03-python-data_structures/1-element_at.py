19c489c986f8cbece612266a9c261f4bdde0b94d
Hlu99 <Hluvuko99@gmail.com>
2023-03-14 14:14:58 -0700

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
-!/usr/bin/python3
+#!/usr/bin/python3
+
+
 def element_at(my_list, idx):
-    for x in my_list:
-        if idx == my_list.index(x):
-            return(x)
-        elif idx > len(my_list):
-            return None
-        elif idx < 0:
-            return None
+    """ Returns an element in a liust"""
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
+    else:
+        return my_list[idx]
+
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
+
+
 def no_c(my_string):
-    l = list(my_string)
-    for x in l:
-        if x == 'c':
-            l.remove('c')
-        if x == 'C':
-            l.remove('C')
-    my_string = "".join(l)
-    return 
+    return ''.join(c for c in my_string if c not in 'cC') 
